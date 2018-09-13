d388a98174d263b0cb3cc4bb22b5cff4f528ec5f
Tony Bowden <tony@tmtm.com>
2018-09-13 10:12:25 +0100

    Also find via eswiki Category

--- scraper.rb
+++ scraper.rb
@@ -3,9 +3,10 @@
 
 require 'wikidata/fetcher'
 
-names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/costa-rica-election-2014', column: 'wikiname')
+en_names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/costa-rica-election-2014', column: 'wikiname')
+es_names = WikiData::Category.new('Categoría:Diputados de Costa Rica 2014-2018', 'es').member_titles
 
 sparq = 'SELECT DISTINCT ?item WHERE { ?item p:P39/ps:P39 wd:Q21328617 }'
 ids = EveryPolitician::Wikidata.sparql(sparq)
 
-EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { en: names })
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { en: en_names, es: es_names })
