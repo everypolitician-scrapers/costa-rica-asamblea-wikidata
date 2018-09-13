561978039603b8846bb898671f70f16ed04053fa
Tony Bowden <tony@tmtm.com>
2018-09-13 10:10:15 +0100

    Also find via SPARQL

--- scraper.rb
+++ scraper.rb
@@ -4,4 +4,8 @@
 require 'wikidata/fetcher'
 
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/costa-rica-election-2014', column: 'wikiname')
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names }, output: false)
+
+sparq = 'SELECT DISTINCT ?item WHERE { ?item p:P39/ps:P39 wd:Q21328617 }'
+ids = EveryPolitician::Wikidata.sparql(sparq)
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { en: names })
